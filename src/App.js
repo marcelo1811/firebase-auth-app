4138610b17f893d6df01d2ee6642e34c38120daa
marcelo1811 <miyachimarcelo@gmail.com>
2021-03-12 09:27:36 -0300

    feat: add initial components with Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router } from '@reach/router'
+import { ProfilePage } from "./Components/ProfilePage"
+import { SignIn } from './Components/SignIn'
+import { SignUp } from './Components/SignUp'
+import { PasswordReset } from './Components/PasswordReset'
 
 function App() {
+  const user = null
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    user ? (
+      <ProfilePage />
+    ) : (
+      <Router>
+        <SignUp path='signUp' />
+        <SignIn path='/'/>
+        <PasswordReset path='passwordReset' />
+      </Router>
+    )
+  )
 }
 
 export default App;
---
+++ src/Components/PasswordReset.js
@@ -0,0 +1,5 @@
+export function PasswordReset() {
+  return (
+    <div>PasswordReset</div>
+  )
+}
